6589e8bedf6f86e792dfbf52b90bd9efe28a0154
eone <vanya.yarovoy@gmail.com>
2020-01-28 13:51:41 +0200

    Messages added

--- test_cred_alert.sh
+++ test_cred_alert.sh
@@ -5,24 +5,24 @@ function fail_test {
   exit 1
 }
 
-echo -e "\nTest case 0. File not exists."
+echo -e "\nTest case 0. Expected: File not exists."
 ./credentials_alert.sh -file ./test-config/notExist && fail_test
 
-echo -e "\nTest case 1. Found unsafe cred. Exit with error"
+echo -e "\nTest case 1. Expected: Found unsafe cred. Exit with error"
 ./credentials_alert.sh -file ./test-config/test-case-1-unsafe-cred.yml && fail_test
 
-echo -e "\nTest case 2. Found unsafe cred. Exit with error"
+echo -e "\nTest case 2. Expected: Found unsafe cred. Exit with error"
 ./credentials_alert.sh -file ./test-config/test-case-2-composite-key-unsafe-cred.yml && fail_test
 
-echo -e "\nTest case 3. Cred is safe. Exit with 0"
-./credentials_alert.sh -file ./test-config/test-case-3-safe-cred.yml || fail_test 
+echo -e "\nTest case 3. Expected: Cred is safe. Exit with 0"
+./credentials_alert.sh -file ./test-config/test-case-3-safe-cred.yml || fail_test
 
-echo -e "\nTest case 4. False positive. Exit with 0"
+echo -e "\nTest case 4. Expected: False positive scenario. Exit with 0"
 ./credentials_alert.sh -file ./test-config/test-case-4-false-positive.yml || fail_test
 
 echo -e "\n"
-echo "=========================="
-echo "===All tests are Green!==="
-echo "=========================="
+echo "==========================="
+echo "===All tests have passed==="
+echo "==========================="
 
 exit 0
